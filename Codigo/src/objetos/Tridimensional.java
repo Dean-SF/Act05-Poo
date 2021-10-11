969eaac613e7bd29f9ff7bcb2f3595e16e02d20d
demoralesv <86642624+demoralesv@users.noreply.github.com>
2021-10-11 12:50:56 -0600

    Clases cilindro y tridimensional

--- Codigo/src/objetos/Tridimensional.java
+++ Codigo/src/objetos/Tridimensional.java
@@ -1,5 +1,8 @@
 package objetos;
 
-public abstract class Tridimensional {
+public abstract class Tridimensional extends Figuras{
+    protected double area;
     
+    public abstract void setArea(double area);
+    public abstract double getArea();
 }
