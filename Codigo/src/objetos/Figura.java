c47af3e891033471d68790f1759e8e7e3c3e338f
EstebanPP <81255764+EstebanPP@users.noreply.github.com>
2021-10-11 12:40:06 -0600

    Cambio de nombre a Figura

--- Codigo/src/objetos/Figura.java
+++ Codigo/src/objetos/Figura.java
@@ -1,10 +1,10 @@
 package objetos;
 
-public abstract class Figuras {
+public abstract class Figura {
     protected double perimetro;
     protected int codigo;
     
-    public void Figura(){}
+    public Figura(){}
 
     public abstract void setPerimetro(double perimetro);
     public abstract void setCodigo(int codigo);
